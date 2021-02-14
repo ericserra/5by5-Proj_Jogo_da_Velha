7ea2e2283c84841bc138d20ab758650b7f664cd9
ericserra <78964631+ericserra@users.noreply.github.com>
2021-02-14 16:51:21 -0300

    Projeto Jogo da Velha

---
+++ Projeto_5by5_Jogo-da-Velha/Projeto_5by5_Jogo-da-Velha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_5by5_Jogo_da_Velha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
